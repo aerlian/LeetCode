7e5684c207ccc7fb3341cc37727b6cf129df727f
Steve <steve.rodgers@gmail.com>
2021-02-13 21:46:53 -0500

    Initial commit

---
+++ DynamicProgramming/LongestPalindrome.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Main.DynamicProgramming
+{
+    /// <summary>
+    /// https://leetcode.com/problems/longest-palindromic-substring/
+    /// </summary>
+    public class LongestPalindrome
+    {
+        public static void Execute()
+        {
+            Console.WriteLine(LongestPalindromeImpl());
+        }
+
+        public static int LongestPalindromeImpl()
+        {
+            return 0;
+        }
+    }
+}

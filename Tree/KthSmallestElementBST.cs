6156fc153e7aba46519bdfe3d4eed17c0a0735f3
aerlian <steve.rodgers@gmail.com>
2021-06-20 19:13:47 -0400

    done (#26)

---
+++ Tree/KthSmallestElementBST.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Main.Common;
+
+namespace Main.Tree
+{
+    /// <summary>
+    /// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+    /// </summary>
+    public class KthSmallestElementBST
+    {
+        public static void Execute()
+        {
+            var tree = TreeNode.BuildTreeFromData(new object[] { 3, 1, 4, null, 2 });
+            var list = new List<int>();
+            KthSmallestElementBSTImpl(tree, list);
+            Console.WriteLine(list[3]);
+        }
+
+        public static void KthSmallestElementBSTImpl(TreeNode node, List<int> list)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            KthSmallestElementBSTImpl(node.left, list);
+            list.Add(node.val);
+            KthSmallestElementBSTImpl(node.right, list);
+        }
+    }
+}

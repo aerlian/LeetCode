6c9912e94eb18f522b03d7ddf81ceb81790182e1
aerlian <steve.rodgers@gmail.com>
2021-06-21 18:47:28 -0400

    done (#29)

---
+++ SeventyFive/Graph/CloneGraph.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Main.SeventyFive.Graph
+{
+    /// <summary>
+    /// https://leetcode.com/problems/clone-graph/
+    /// </summary>
+    public class CloneGraph
+    {
+        public CloneGraph()
+        {
+        }
+
+        public Node CloneGraphImpl(Node node, IDictionary<int, Node> cloneMap)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            if (cloneMap.ContainsKey(node.val))
+            {
+                return cloneMap[node.val];
+            }
+
+            var adjList = new List<Node>();
+            var clone = new Node(node.val, adjList);
+
+            cloneMap.Add(node.val, clone);
+
+            foreach (var n in node.neighbors)
+            {
+                if (cloneMap.ContainsKey(n.val))
+                {
+                    adjList.Add(cloneMap[n.val]);
+                }
+                else
+                {
+                    adjList.Add(CloneGraphImpl(n, cloneMap));
+                }
+            }
+
+            return clone;
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public IList<Node> neighbors;
+
+        public Node()
+        {
+            val = 0;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val, List<Node> _neighbors)
+        {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+}
---
+++ SeventyFive/TwoPointers/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Main.SeventyFive.TwoPointers
+{
+    public class BestTimeToBuyAndSellStock
+    {
+        public static void Execute()
+        {
+            Console.WriteLine(MaxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            var l = 0;
+            var r = 1;
+
+            var maxProfit = 0;
+
+            while (r < prices.Length)
+            {
+                if (prices[r] - prices[l] > 0)
+                {
+                    var profit = prices[r] - prices[l];
+                    maxProfit = Math.Max(maxProfit, profit);
+                }
+                else
+                {
+                    l = r;
+                }
+
+                r += 1;
+            }
+
+            return maxProfit;
+        }
+    }
+}

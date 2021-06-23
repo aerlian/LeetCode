89ec7e8694805b47c9da97799e2c0ca1aa01fce4
Steve <steve.rodgers@gmail.com>
2021-06-22 20:47:22 -0400

    done

---
+++ Common/IntervalExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Main.Common
+{
+    public static class IntervalExtensions
+    {
+        public static string Dump(this int [][] intervals)
+        {
+            return string.Join(",", intervals.Select(i => Dump(i)));
+        }
+
+        public static string Dump(this int[] interval)
+        {
+            return $"({interval[0]}:{interval[1]})";
+        }
+    }
+}

758690dab959970dfd116331a657c4ee2ef71d3d
aerlian <steve.rodgers@gmail.com>
2021-06-27 13:49:35 -0400

    done (#35)

--- SeventyFive/Strings/PalindromeSubstrings.cs
+++ SeventyFive/Strings/PalindromeSubstrings.cs
@@ -8,32 +8,7 @@ namespace Main.SeventyFive.Strings
             Console.WriteLine(CountSubstrings("fdsklf"));
         }
 
-        public static int CountPalindromeOdd(string source, int offset)
-        {
-            var l = offset;
-            var r = offset;
-
-            var count = 0;
-
-            while (l >= 0 && r < source.Length)
-            {
-                if (source[l] == source[r])
-                {
-                    count += 1;
-                }
-                else
-                {
-                    break;
-                }
-
-                l -= 1;
-                r += 1;
-            }
-
-            return count;
-        }
-
-        public static int CountPalindromeEven(string source, int left, int right)
+        public static int CountPalindrome(string source, int left, int right)
         {
             var l = left;
             var r = right;
@@ -64,8 +39,8 @@ namespace Main.SeventyFive.Strings
 
             for (var i = 0; i < s.Length; i++)
             {
-                count += CountPalindromeOdd(s, i);
-                count += CountPalindromeEven(s, i, i + 1);
+                count += CountPalindrome(s, i, i);
+                count += CountPalindrome(s, i, i + 1);
             }
 
             return count;

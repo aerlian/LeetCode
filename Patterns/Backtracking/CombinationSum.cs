55b7310c6f306b9a84b63d1dbcaa9e49c2585a22
Steve <steve.rodgers@gmail.com>
2021-07-04 15:50:09 -0400

    done

--- Patterns/Backtracking/CombinationSum.cs
+++ Patterns/Backtracking/CombinationSum.cs
@@ -12,30 +12,54 @@ namespace Main.Patterns.Backtracking
         public static void Execute()
         {
             IList<IList<int>> output = new List<IList<int>>();
-            CombinationSumImpl(new int[] { 2, 3, 5 }, 0, 8, new List<int>(), output);
+            dfs(new int[] { 2, 3, 5 }, 0, new List<int>(), 0, 8, output);
             Console.WriteLine(string.Join(Environment.NewLine, output.Select(rng => string.Join(",", rng))));
         }
 
-        public static void CombinationSumImpl(int[] candidates, int offset, int target, IList<int> candidateList, IList<IList<int>> output)
+        static void dfs(int[] candidates, int offset, List<int> cur, int total, int target, IList<IList<int>> output)
         {
-            if (target < 0)
+            if (total == target)
+            {
+                output.Add(new List<int>(cur));
+                return;
+            }
+
+            if (total > target)
             {
                 return;
             }
 
-            if (target == 0)
+            if (offset == candidates.Length)
             {
-                output.Add(new List<int>(candidateList));
                 return;
             }
 
-            for(var i = offset; i < candidates.Length; i++)
-            {
-                var current = candidates[i];
-                candidateList.Add(current);
-                CombinationSumImpl(candidates, i, target - current, candidateList, output);
-                candidateList.RemoveAt(candidateList.Count - 1);
-            }
+            cur.Add(candidates[offset]);
+            dfs(candidates, offset, cur, total + candidates[offset], target, output);
+            cur.RemoveAt(cur.Count - 1);
+            dfs(candidates, offset + 1, cur, total, target, output);
         }
+
+        //public static void CombinationSumImpl(int[] candidates, int offset, int target, IList<int> candidateList, IList<IList<int>> output)
+        //{
+        //    if (target < 0)
+        //    {
+        //        return;
+        //    }
+
+        //    if (target == 0)
+        //    {
+        //        output.Add(new List<int>(candidateList));
+        //        return;
+        //    }
+
+        //    for(var i = offset; i < candidates.Length; i++)
+        //    {
+        //        var current = candidates[i];
+        //        candidateList.Add(current);
+        //        CombinationSumImpl(candidates, i, target - current, candidateList, output);
+        //        candidateList.RemoveAt(candidateList.Count - 1);
+        //    }
+        //}
     }
 }
